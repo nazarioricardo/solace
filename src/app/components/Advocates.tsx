63148fcbdca7cf722a7008a4a369986447bffaed
Ricardo Nazario <nazarioricardo@gmail.com>
2025-03-08 14:53:06 -0400

    fix: moved filter logic to render

--- src/app/components/Advocates.tsx
+++ src/app/components/Advocates.tsx
@@ -10,14 +10,12 @@ type Advocates = Advocate[];
 
 function Advocates() {
   const [advocates, setAdvocates] = useState<Advocates>([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocates>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
 
   const fetchAdvocates = async () => {
     const response = await fetch("/api/advocates");
     const jsonResponse = await response.json();
     setAdvocates(jsonResponse.data);
-    setFilteredAdvocates(jsonResponse.data);
   };
 
   useEffect(() => {
@@ -28,20 +26,6 @@ function Advocates() {
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setSearchTerm(value);
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
   };
 
   const onSubmitForm = (event: FormEvent<HTMLFormElement>) => {
@@ -49,10 +33,21 @@ function Advocates() {
     console.log("submitting form...");
   };
 
+  const filteredAdvocates = advocates.filter((advocate) => {
+    return (
+      advocate.firstName.includes(searchTerm) ||
+      advocate.lastName.includes(searchTerm) ||
+      advocate.city.includes(searchTerm) ||
+      advocate.degree.includes(searchTerm) ||
+      advocate.specialties.includes(searchTerm) ||
+      String(advocate.yearsOfExperience).includes(searchTerm)
+    );
+  });
+
   return (
     <div>
       <SearchForm onChange={onChange} onSubmit={onSubmitForm} />
-      <Table advocates={advocates} />
+      <Table advocates={filteredAdvocates} />
     </div>
   );
 }
